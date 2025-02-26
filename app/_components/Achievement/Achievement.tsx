9f74cad03a07c42d8c151d5e098ed4d11de6efd0
tatsuhikoArakawa <arakawa_tatsuhiko_bp@beenos.com>
2025-02-26 22:12:15 +0900

    キャリアの追加

--- app/_components/Achievement/Achievement.tsx
+++ app/_components/Achievement/Achievement.tsx
@@ -2,62 +2,6 @@ import styles from "@/app/_components/Achievement/Achievement.module.scss";
 import MainAchievementCard from "@/app/_components/Achievement/MainAchievementCard";
 import SubAchievementCard from "@/app/_components/Achievement/SubAchievementCard";
 
-// const schoolList = [
-//   {
-//     id: 1,
-//     name: "大阪芸術大学 芸術学部 文芸学科",
-//     term: "2012 ~ 2017",
-//     link: "https://www.osaka-geidai.ac.jp/departments/literaryarts",
-//     details: [
-//       {
-//         id: 1,
-//         title: "大阪NHK 竹内街道１４００年祭「芸術と歴史街道」へ短歌の展示",
-//         link: "https://www.osaka-geidai.ac.jp/topics/takeuchi_topics_2013",
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     name: "Grossmont College Computer Science Infomation System",
-//     term: "2020 ~ 2021",
-//     link: "https://www.grossmont.edu/academics/programs/csis/index.php",
-//     details: [],
-//   },
-// ];
-// const careerList = [
-//   {
-//     id: 1,
-//     name: "株式会社クロコ ITシステム開発事業部 フルスタックエンジニア",
-//     term: "2021 ~ 2023",
-//     link: "https://kuroco.jp/",
-//     details: [
-//       {
-//         id: 1,
-//         title: "社員インタビュー",
-//         link: "https://interview.kuroco.jp/it/20220413-572/",
-//       },
-//       {
-//         id: 2,
-//         title: "社員総会司会担当",
-//         link: "",
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     name: "株式会社テコテック 次世代開発事業部 フロントエンジニア",
-//     term: "2023 ~ 現在",
-//     link: "https://www.tecotec.co.jp/",
-//     details: [
-//       {
-//         id: 1,
-//         title: "United uniqueness賞 受賞（リンク先、受賞者写真右上）",
-//         link: "https://www.tecotec.co.jp/recruit/news/20241223.html",
-//       },
-//     ],
-//   },
-// ];
-
 const achievementList = [
   {
     id: 1,
