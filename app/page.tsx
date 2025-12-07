65a581f91dd830288e5090741b6a71ba31407413
TatsuhikoA0314 <tatsualabs0314@gmail.com>
2025-12-07 14:38:38 +0900

    upgrade: ライブラリの更新とsafe-chainの導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Achievement from "app/_domain/Achievement/Achievement";
-import HomeTitle from "app/_domain/HomeTitle";
 import Award from "@/app/_domain/Award/Award";
 import Skill from "@/app/_domain/Skill/Skill";
 import styles from "@/app/page.module.scss";
 import { fetchSheetData } from "@/utils/fetchSheetData";
+import Achievement from "app/_domain/Achievement/Achievement";
+import HomeTitle from "app/_domain/HomeTitle";
 
 export type AwardSheet = {
 	data: {
